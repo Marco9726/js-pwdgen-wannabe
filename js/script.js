46d5941e8c2066f0fd54ae526cebb883670103b6
Marco 9726 <marcosarra9@gmail.com>
2022-11-24 17:15:23 +0100

    fix parola "inseirisci"

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 //1 - Chiedere il nome
-let nome = prompt('Inseirisci il tuo nome');
+let nome = prompt('Inserisci il tuo nome');
 //2 - Chiedere il cognome
-let cognome = prompt('Inseirisci il tuo cognome');
+let cognome = prompt('Inserisci il tuo cognome');
 //3 - Chiedere il colore preferito
-let colore = prompt('Inseirisci il tuo colore preferito');
+let colore = prompt('Inserisci il tuo colore preferito');
 //4 - Definire il valore 21
 const numb = 21;
 //5 - Inserisco ivalori nell'HTML
