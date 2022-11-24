b47b0d3df85a1debbd8f103aeb7a0cd63660d383
Marco 9726 <marcosarra9@gmail.com>
2022-11-24 17:02:18 +0100

    esercizio finito (?)

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//1 - Chiedere il nome
+let nome = prompt('Inseirisci il tuo nome');
+//2 - Chiedere il cognome
+let cognome = prompt('Inseirisci il tuo cognome');
+//3 - Chiedere il colore preferito
+let colore = prompt('Inseirisci il tuo colore preferito');
+//4 - Definire il valore 21
+const numb = 21;
+//5 - Inserisco ivalori nell'HTML
+document.getElementById('Password').innerHTML = nome + cognome + colore + numb;
